577d2d866521e1a96c5b9da17c89098e51d77005
Sebastian <sebastian5253@wp.pl>
2022-10-25 20:48:53 +0200

    [INPL-1219] Create prototype header
    Create first prototype header in new management 2.0
    application. Add dashboard templates.
    
    [INP-111]

---
+++ src/dashboard/dashboard.js
@@ -0,0 +1,54 @@
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+
+const user = {
+    name: 'Tom Cook',
+    email: 'tom@example.com',
+    imageUrl:
+        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+}
+const navigation = [
+    { name: 'Dashboard', href: '#', current: true },
+    { name: 'Team', href: '#', current: false },
+    { name: 'Projects', href: '#', current: false },
+    { name: 'Calendar', href: '#', current: false },
+    { name: 'Reports', href: '#', current: false },
+]
+const userNavigation = [
+    { name: 'Your Profile', href: '#' },
+    { name: 'Settings', href: '#' },
+    { name: 'Sign out', href: '#' },
+]
+
+
+export default function Dashboard() {
+    return (
+        <>
+            {/*
+        This example requires updating your template:
+
+        ```
+        <html class="h-full bg-gray-100">
+        <body class="h-full">
+        ```
+      */}
+            <div className="min-h-full">
+                <header className="bg-white shadow">
+                    <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+                        <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+                    </div>
+                </header>
+                <main>
+                    <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+                        {/* Replace with your content */}
+                        <div className="px-4 py-6 sm:px-0">
+                            <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
+                        </div>
+                        {/* /End replace */}
+                    </div>
+                </main>
+            </div>
+        </>
+    )
+}
