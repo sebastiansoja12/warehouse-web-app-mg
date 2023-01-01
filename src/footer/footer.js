7370b47d992d862ab8271cfaf723912acb24de62
Sebastian <sebastian5253@wp.pl>
2023-01-01 18:11:20 +0100

    Update version

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
     return (
         <footer className="footer footer-center p-4 bg-base-300 text-base-content">
             <div>
-                <p>Inparcel 2.0 - Development version 2022.15</p>
+                <p>Inparcel 2.0 - Development version 2023.1</p>
             </div>
         </footer>
     )
